767b21e95abc70c3af20b216d98816441c24f765
Varun Gore <vgoredev@gmail.com>
2015-03-22 03:27:29 -0700

    skeleton

--- src/DHCPLeaseManagement.cpp
+++ src/DHCPLeaseManagement.cpp
@@ -11,26 +11,53 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <unistd.h>
 using namespace std;
+
+struct ip_entry{
+	string ip;
+	int expiration_time;
+	ip_entry *ptr;
+};
 
 int main() {
 
-	string line, token, ip, lease_time;
-	int time = 0;
+	ip_entry *head;
+	string line, token, ip, lease_time, sleep_time, temp;
+	int t1 = 0;
+	time_t now = time(0);
+
+
+	//Move to a function
 	ifstream myfile("input.txt");
 	if (myfile.is_open()) {
 		while (getline(myfile, line)) {
-			cout << line << '\n';
+
+			now = time(0);
+			cout << now;
+
 			istringstream strm(line);
 
-			cout << token << endl;
+			cout << now << endl;
+
 			if(line[0] == 'P'){
-				//sleep
+				strm >> temp;// just to iterate to sleep time
+				strm >> sleep_time;
+				t1 = stoi(sleep_time);
+				//sleep(t1);
 			}else{
 				strm >> ip;
 				strm >> lease_time;
-				time = stoi(lease_time);
-				cout << time;
+				t1 = stoi(lease_time);
+				//cout << t1;
+				ip_entry *e1 = new ip_entry();
+				e1->ip = ip;
+				e1->expiration_time = t1+now;
+				if(head == NULL)
+					head =e1;
+				else{
+					//add at proper location
+				}
 			}
 
 		}
