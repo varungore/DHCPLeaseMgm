437cde1570e05ed6a080842918a78234ec6a6fca
Varun Gore <vgoredev@gmail.com>
2015-03-22 15:27:15 -0700

    file read and ll created

--- src/DHCPLeaseManagement.cpp
+++ src/DHCPLeaseManagement.cpp
@@ -14,22 +14,37 @@
 #include <unistd.h>
 using namespace std;
 
-struct ip_entry{
+void read_input_ips();
+void print_ll();
+
+struct ip_entry {
 	string ip;
 	int expiration_time;
-	ip_entry *ptr;
+	ip_entry *next = NULL;
 };
+ip_entry *head;
+
 
 int main() {
+	read_input_ips();
+	print_ll();
+}
 
-	ip_entry *head;
+void print_ll(){
+	ip_entry *current_node = head;
+	while(current_node != NULL){
+		cout << "printing " << current_node->ip << " :: " << current_node->expiration_time << endl;
+		current_node = current_node->next;
+	}
+}
+void read_input_ips() {
+
 	string line, token, ip, lease_time, sleep_time, temp;
 	int t1 = 0;
 	time_t now = time(0);
 
-
-	//Move to a function
 	ifstream myfile("input.txt");
+
 	if (myfile.is_open()) {
 		while (getline(myfile, line)) {
 
@@ -38,31 +53,42 @@ int main() {
 
 			istringstream strm(line);
 
-			cout << now << endl;
+			cout << now;
 
-			if(line[0] == 'P'){
-				strm >> temp;// just to iterate to sleep time
+			if (line[0] == 'P') {
+				strm >> temp; // just to iterate to sleep time
 				strm >> sleep_time;
 				t1 = stoi(sleep_time);
 				//sleep(t1);
-			}else{
+			} else {
 				strm >> ip;
+				cout << " " << ip << endl;
 				strm >> lease_time;
 				t1 = stoi(lease_time);
-				//cout << t1;
 				ip_entry *e1 = new ip_entry();
 				e1->ip = ip;
-				e1->expiration_time = t1+now;
-				if(head == NULL)
-					head =e1;
-				else{
-					//add at proper location
+				e1->expiration_time = t1 + now;
+				if (head == NULL)
+					head = e1;
+				else {
+					ip_entry *next_node, *current_node;
+					next_node = head;
+					while(next_node != NULL && next_node->expiration_time < e1->expiration_time){
+						current_node = next_node;
+						next_node = next_node->next;
+					}
+
+					e1->next = next_node;
+					if(next_node == head)
+						head = e1;
+					else
+						current_node->next = e1;
+
 				}
 			}
 
 		}
 		myfile.close();
 	}
-
-
 }
+
