ab111a5e4f118265fc96ad9db55bdd212a6e5978
Varun Gore <vgoredev@gmail.com>
2015-03-22 15:40:20 -0700

    delete head

--- src/DHCPLeaseManagement.cpp
+++ src/DHCPLeaseManagement.cpp
@@ -16,17 +16,22 @@ using namespace std;
 
 void read_input_ips();
 void print_ll();
+void remove_inactive_ips();
 
 struct ip_entry {
 	string ip;
 	int expiration_time;
-	ip_entry *next = NULL;
+	ip_entry *next;
 };
+
 ip_entry *head;
 
 
 int main() {
 	read_input_ips();
+	print_ll();
+	//sleep(1);
+	remove_inactive_ips();
 	print_ll();
 }
 
@@ -37,6 +42,23 @@ void print_ll(){
 		current_node = current_node->next;
 	}
 }
+
+void remove_inactive_ips(){
+	ip_entry *current_node = head;
+	time_t now = time(0);
+	while(1){
+		cout << "in while";
+		while(current_node != NULL){
+			if(current_node->expiration_time <= now){
+				cout << "Deleting head\n";
+				head = current_node->next;
+				delete current_node;
+				current_node = head;
+			}
+		}
+	}
+}
+
 void read_input_ips() {
 
 	string line, token, ip, lease_time, sleep_time, temp;
