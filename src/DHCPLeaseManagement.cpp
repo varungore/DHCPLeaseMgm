2212b044536c9426aaa30b2045838b5f0d31be1b
Varun Gore <vgoredev@gmail.com>
2015-03-22 20:33:34 -0700

    updated with thread and mutex

--- src/DHCPLeaseManagement.cpp
+++ src/DHCPLeaseManagement.cpp
@@ -12,12 +12,19 @@
 #include <string>
 #include <sstream>
 #include <unistd.h>
+#include <thread>
+#include <mutex>
+#include <map>
+
+std::map<std::string, int> ip_map;
+
+std::mutex thread_mutex;
 
 using namespace std;
 
-void read_input_ips();
+void* read_input_ips(void*);
 void print_ll();
-void remove_inactive_ips();
+void* remove_inactive_ips(void*);
 
 struct ip_entry {
 	string ip;
@@ -27,96 +34,112 @@ struct ip_entry {
 
 ip_entry *head;
 
-
 int main() {
-	read_input_ips();
-	cout<<"\n\n\n";
-	print_ll();
-	//sleep(1);
-	remove_inactive_ips();
+	pthread_t threads[2];
+
+	pthread_create(&threads[0], NULL, read_input_ips, NULL);
+	pthread_create(&threads[1], NULL, remove_inactive_ips, NULL);
+	(void) pthread_join(threads[0], NULL);
+	(void) pthread_join(threads[1], NULL);
 	print_ll();
 }
 
-void print_ll(){
+void print_ll() {
+	thread_mutex.lock();
 	ip_entry *current_node = head;
-	while(current_node != NULL){
-		cout << "printing " << current_node->ip << " :: " << current_node->expiration_time << endl;
+	while (current_node != NULL) {
+		cout << "\nprinting " << current_node->ip << " :: "
+				<< current_node->expiration_time << endl;
 		current_node = current_node->next;
 	}
+	thread_mutex.unlock();
 }
 
-void remove_inactive_ips(){
-	ip_entry *current_node = head;
+void *remove_inactive_ips(void* ptr) {
+
+	ip_entry *current_node;
 	time_t now = time(0);
-	while(1){
-
-
-		//cout << "in while";
-		cout<< "asf" << now<<endl;
-		while(current_node != NULL){
+	while (1) {
+		thread_mutex.lock();
+		current_node = head;
 		time_t now = time(0);
-		//cout<<"while remove\n";
-			if(current_node->expiration_time <= now){
-				cout << "Deleting head:";
-				cout<<current_node->ip<<endl;
-				head = current_node->next;
-				//delete current_node;
-				current_node = head;
-
-			}
+		while (current_node != NULL && current_node->expiration_time <= now) {
+			cout << "\nDeleting head:";
+			cout << current_node->ip << endl;
+			head = current_node->next;
+			current_node = head;
 		}
+		thread_mutex.unlock();
 	}
 }
 
-void read_input_ips() {
+void *read_input_ips(void*) {
 
 	string line, token, ip, lease_time, sleep_time, temp;
 	int t1 = 0;
 	time_t now = time(0);
 
-	ifstream myfile("ager_input.txt");
+	ifstream myfile("input.txt");
 
 	if (myfile.is_open()) {
 		while (getline(myfile, line)) {
 
 			now = time(0);
-			cout << now;
 
 			istringstream strm(line);
-
-			cout << now;
 
 			if (line[0] == 'P') {
 				strm >> temp; // just to iterate to sleep time
 				strm >> sleep_time;
-				t1 = stoi(sleep_time);
-				//sleep(t1);
+
+				t1 = stoi(sleep_time) / 1000;
+				thread_mutex.lock(); //lock to clearly print
+				cout << "\nSleeping for " << t1 << "\n";
+				thread_mutex.unlock();
+				sleep(t1);
 			} else {
 				strm >> ip;
-				cout << " " << ip << endl;
+				cout << now << " " << ip << endl;
 				strm >> lease_time;
 				t1 = stoi(lease_time);
 				ip_entry *e1 = new ip_entry();
 				e1->ip = ip;
-				e1->expiration_time = (t1/1000) + now;
+				e1->expiration_time = (t1 / 1000) + now;
 				e1->next = NULL;
-				if (head == NULL)
+
+				thread_mutex.lock();
+
+				if (head == NULL) {
 					head = e1;
-				else {
+					ip_map[ip] = 1;
+				} else {
 					ip_entry *next_node, *current_node;
 					next_node = head;
-					while(next_node != NULL && next_node->expiration_time < e1->expiration_time){
+					if (ip_map[ip] == 1) {
+						//delete existing entry
+						while (next_node != NULL ) {
+							if(next_node->ip == ip){
+								current_node->next = next_node->next;
+								delete next_node;
+								next_node = current_node->next;
+							}
+						}
+					}
+					//add ip with new lease time// Redundant can be optimized
+					next_node = head;
+					while (next_node != NULL
+							&& next_node->expiration_time < e1->expiration_time) {
 						current_node = next_node;
 						next_node = next_node->next;
 					}
-
 					e1->next = next_node;
-					if(next_node == head)
+					if (next_node == head) {
 						head = e1;
-					else
+					} else {
 						current_node->next = e1;
-
+					}
 				}
+				thread_mutex.unlock();
 			}
 
 		}
